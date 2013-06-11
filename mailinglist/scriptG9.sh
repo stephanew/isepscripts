8e80da3847f3d3fec0d6adeaf5efd3898e68744f
Romain Paulus <rmnx@hotmail.fr>
2013-06-11 10:15:57 +0200

    Completed all objectives

--- mailinglist/scriptG9.sh
+++ mailinglist/scriptG9.sh
@@ -2,13 +2,13 @@
 
 
 # OBJECTIFS (n'hesitez pas a en rajouter d'autres !)
-# - Compter le nombre de mots
-# - Compter le nombre moyen de mots par mail
-# - Trouver les mots les plus courants
-# - Liste de tous les expediteurs
-# - Nombre de mails envoyés par chacun
-# - Nombre de mails envoyés par mois
-# - Les mois où le plus d'emails ont été envoyés
+# - Compter le nombre de mots [OK]
+# - Compter le nombre moyen de mots par mail [OK]
+# - Trouver les mots les plus courants [OK]
+# - Liste de tous les expediteurs [OK]
+# - Nombre de mails envoyés par chacun [OK]
+# - Nombre de mails envoyés par mois [OK]
+# - Les mois où le plus d'emails ont été envoyés [OK]
 
 # FONCTION UTILE
 function pause(){
@@ -25,7 +25,7 @@ else
 	# --EXPEDITEURS
 	echo "-- EXPEDITEURS"
 	echo " "
-	grep "(?<=^From: ).+(?= at)" $1 -Po | sort -rn -k1 | uniq -c | sort -rn -k1 | head -n 15
+	grep "(?<=^From: )[\w]+ at [\w\.]+" $1 -Po | sort -rn -k1 | uniq -c | sort -rn -k1 | head -n 15 | sed 's/ at /@/g' 
 	pause
 
 	# --COMPTAGE DES MOTS
@@ -34,10 +34,29 @@ else
 
 	# La regex de grep correspond à toutes les chaines de caractères qui ne contiennent pas d'espaces
 	grep "[^\s]+" $1 -Po | sort -rn -k1 | uniq -c | sort -rn -k1 | head -n 15
-	echo "TOTAL:`wc -w < $1`"
+
+	# La commande Sed permet d'enlever les espaces au début du résultat
+	TOTALWORDS=`wc -w < $1 | sed -e 's/^[ \t]*//'`
+	echo "TOTAL: $TOTALWORDS"
+	pause
+
+	echo "-- NOMBRE DE MOTS PAR MAIL"
 	echo " "
 
-	# Nombre de mots par mails
-	#grep "[^\s]+" $1 -Po | wc -l
+	# -- NOMBRE DE MOTS PAR MAILS
+	COUNTMAILS=`grep "(?<=^From: ).+(?= at)" $1 -Po | wc -l | sed -e 's/^[ \t]*//'`
+	# La commande bc permet d'effectuer l'opération de division
+	echo $(echo "$TOTALWORDS / $COUNTMAILS" | bc -l )
+	pause
+
+	# -- MAILS PAR MOIS
+	echo "-- MOIS D'ENVOIS"
+	echo " "
+	grep "^From .+ at .+$" $1 -Po | grep "(?<=(Sun|Mon|Tue|Wed) )[\w]+(?= \d)" -Po | sort -rn -k1 | uniq -c | sort -rn -k1 | head -n 15
+	pause
+
+
+
+
 
 fi
