8af84fd1e817d23f7240f1f09b03cb615c98c739
slyfer974 <your_email@example.com>
2013-06-16 10:22:30 -0700

    Revert "Adding of morecommented.sh wich print the top5 most commented articles, and the content"
    This reverts commit c096feb95f96f3050a96709ec186d68e88513e69.

--- groupevdm/comptage_vdm.sh
+++ groupevdm/comptage_vdm.sh
@@ -1,7 +1,7 @@
 #Ces 2 morceaux de codes permettent le comptage du mot VDM dans tous les fichiers
 #version longue
 #!/bin/bash
-compte=0;
+$compte=0;
 for j in `grep "VDM" *`
 do
 compte=$((compte + 1));
